62839a99cb6b8fee43250b18ad6c4c778ed3a2f5
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-11-04 14:45:32 +0000

    refactored parsing args into separate function

---
+++ parse_command.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void parse_command(const char *command,char *args)
+{
+    
+    int arg_count = 0;
+    char *token = strtok((char *)command, " ");
+
+        while (token != NULL)
+        {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_count] = NULL;
+}
