f2c15ac841aed938b9715d379c3cbb9eeae05709
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-11-04 13:33:31 +0000

    added more function prototypes

--- main.h
+++ main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 
 void display_prompt(void);
-
+void read_commands(char *command,size_t size)
+void _print(const char *text)
+void execute_commands(const char *command)
 
 #endif
